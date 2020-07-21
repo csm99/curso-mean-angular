c54f2f504110e0b5d5d89edf18302c5bb20a15e7
chaarlie37 <carlos37sm@gmail.com>
2020-07-21 17:03:46 +0200

    primera parte hecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { MatExpansionModule } from '@angular/material/expansion';
-
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import {PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -8,15 +10,14 @@ import { Post } from '../post.model';
 })
 export class PostCreateComponent{
 
-  enteredContent = '';
-  enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost(){
-    const newPost: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
-    };
-    this.postCreated.emit(newPost);
+  constructor(public postsService: PostsService) {
+  }
+
+  onAddPost(form: NgForm){
+    if (form.valid){
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
+    }
   }
 }
